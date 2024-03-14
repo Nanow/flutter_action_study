53b45910161ea96047ad9b6853b65fea96e7283e
Nanow <Nanow@users.noreply.github.com>
2024-03-14 18:01:18 +0000

    deploy: 626ee20771c8346760fe05f8e4214af104bc7287

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "9a8beb6fd2cdcb157425a09d1499a124",
-"/": "9a8beb6fd2cdcb157425a09d1499a124",
+const RESOURCES = {"index.html": "c4d53395e6e30c04deafd550634ce789",
+"/": "c4d53395e6e30c04deafd550634ce789",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
