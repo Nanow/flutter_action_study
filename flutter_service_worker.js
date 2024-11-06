36f2a656c958cc219c416d317143a32fe1bb0736
Nanow <Nanow@users.noreply.github.com>
2024-11-06 15:12:13 +0000

    deploy: 3bdb9f679f9ced33fd0d1517609be0e7069229c2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "0d5699c7917aee0964a64e73d37a56a9",
+"main.dart.js": "ea8c1d3cb9852154b07ed8c80bca094a",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/fonts/MaterialIcons-Regular.otf": "8ea08ea2444cc58ec5807fd7669e488f",
@@ -13,8 +13,8 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
-"index.html": "9c1fd4dc81496cdeb680315faf4c6f43",
-"/": "9c1fd4dc81496cdeb680315faf4c6f43",
+"index.html": "172b4de9c7d057529a8ec1d8e07409d7",
+"/": "172b4de9c7d057529a8ec1d8e07409d7",
 "manifest.json": "e1385e49fd82fbbb631acb1831c6f498",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
@@ -32,7 +32,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "88d222b96e2e33a599d878121effea57",
-"flutter_bootstrap.js": "c933cf948852d89feadddffdeb0bb504"};
+"flutter_bootstrap.js": "bbaf2cbd6c159c33eeb7e1b76a1a66af"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
--- main.dart.js
+++ main.dart.js
@@ -18012,7 +18012,7 @@ A.ZU.prototype={
 $1(a){var s=A.cv().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/36335019a8eab588c3c2ea783c618d90505be233/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/db49896cf25ceabc44096d5f088d86414e05a7aa/":s)+a},
 $S:110}
 A.IE.prototype={
 d8(){B.c.R(this.a.a.save())},
